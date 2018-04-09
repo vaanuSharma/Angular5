5b66dd0f6659367a666cbc5b5e26bca35ce0a88f
vandanaSharma <vandana.pdmce@gmail.com>
2018-04-09 12:55:59 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent} from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule
